75e1eb4fe5b9a4f424be8b07a35d657734f32f8f
Leandro Henrique <emtudo@gmail.com>
2021-06-12 22:44:29 -0300

    composer update update version bypass use Bypass::serve

--- tests/Unit/Services/BookCreateFromISBNServiceTest.php
+++ tests/Unit/Services/BookCreateFromISBNServiceTest.php
@@ -7,6 +7,7 @@ use App\Services\BookCreateFromISBNService;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Tests\TestCase;
 use Ciareis\Bypass\Bypass;
+use Ciareis\Bypass\Route;
 use Exception;
 
 class BookCreateFromISBNServiceTest extends TestCase
@@ -16,14 +17,13 @@ class BookCreateFromISBNServiceTest extends TestCase
     public function test_create_book_with_isbn()
     {
         $isbn = "9788538302759";
-        $body = '{"title": "Propósitos (em Portuguese Do Brasil)","title_long": "Portuguese Purpose Driven Life, Para Que Estou Na Terra? Uma Vida Com","8538302752": "string","9788538302759": "string","dewey_decimal": "string","binding": "string","publisher": "string","language": "string","date_published": "2013-02-01T06:00:52.400Z","edition": "string","pages": 0,"dimensions": "string","overview": "string","image": "string","msrp": 0,"excerpt": "string","synopsys": "string","authors": [  "string"],"subjects": [  "string"],"reviews": [  "string"],"prices": [  {    "condition": "string",    "merchant": "string",    "merchant_logo": "string",    "merchant_logo_offset": {      "x": "string",      "y": "string"    },    "shipping": "string",    "price": "string",    "total": "string",    "link": "string"  }],"related": {  "type": "string"}}';
-        $bypass = Bypass::open();
-
-        $bypass->addRoute(method: 'get', uri: "/books/{$isbn}", status: 200, body: $body);
+        $bypass = Bypass::serve(
+            Route::ok(uri: "/books/{$isbn}", body: $this->getBody())
+        );
 
         $service = new BookCreateFromISBNService("token");
 
-        $service->setBaseUrl($bypass->getBaseUrl());
+        $service->setBaseUrl((string) $bypass);
         $response = $service->handle($isbn);
         $this->assertInstanceOf(Book::class, $response);
     }
@@ -36,5 +36,55 @@ class BookCreateFromISBNServiceTest extends TestCase
         $service = new BookCreateFromISBNService("token");
 
         $service->handle($isbn);
+    }
+
+    protected function getBody()
+    {
+        return [
+        "title" => "Propósitos (em Portuguese Do Brasil)",
+        "title_long" => "Portuguese Purpose Driven Life, Para Que Estou Na Terra? Uma Vida Com",
+        "isbn" => "8538302752",
+        "isbn13" => "9788538302759",
+        "dewey_decimal" => "string",
+        "binding" => "string",
+        "publisher" => "string",
+        "language" => "string",
+        "date_published" => "2013-02-01T06:00:52.400Z",
+        "edition" => "string",
+        "pages" => 0,
+        "dimensions" => "string",
+        "overview" => "string",
+        "image" => "string",
+        "msrp" => 0,
+        "excerpt" => "string",
+        "synopsys" => "string",
+        "authors" => [
+          "Rick Warren"
+        ],
+        "subjects" => [
+          "string"
+        ],
+        "reviews" => [
+          "string"
+        ],
+        "prices" => [
+          [
+            "condition" => "string",
+            "merchant" => "string",
+            "merchant_logo" => "string",
+            "merchant_logo_offset" => [
+              "x" => "string",
+              "y" => "string"
+            ],
+            "shipping" => "string",
+            "price" => "string",
+            "total" => "string",
+            "link" => "string"
+          ]
+        ],
+        "related" => [
+            "type" => "string"
+        ]
+        ];
     }
 }
